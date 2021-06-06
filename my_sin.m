b75f664c691a57a994058c5124e4888d8e7db10b
L_DelOff <51275636+LDelOff@users.noreply.github.com>
2021-06-06 15:50:46 +0300

    Update my_sin.m
    + названия осей

--- my_sin.m
+++ my_sin.m
@@ -1,7 +1,13 @@
-%%By Soviet
+%%By Soviet & L_DelOff
 
 A=2;
 f=10;
-t=0:0.01:10
-y=A*sin(f*t);
-plot(t,y) 
+% create figure and axes
+fig=figure;
+ax=axes;
+t=0:0.001:1;
+y=A*sin(2*pi*f*t);
+plot(t,y)
+title('y=sin(2\pi\bulletf\bullett)')
+xlabel('time, s')
+ylabel('amplitude, V')
